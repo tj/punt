8a519a2f6bf452b3493377740631c3308dbc1df3
TJ Holowaychuk <tj@vision-media.ca>
2014-01-08 14:22:22 -0800

    refactor example to use vargs

--- examples/pub.js
+++ examples/pub.js
@@ -3,5 +3,5 @@ var punt = require('..');
 var sock = punt.connect('0.0.0.0:5000');
 
 setInterval(function(){
-  sock.send({ hello: 'world' });
+  sock.send('foo', 'bar');
 }, 10);
--- examples/sub.js
+++ examples/sub.js
@@ -2,6 +2,6 @@
 var punt = require('..');
 var sock = punt.bind('0.0.0.0:5000');
 
-sock.on('message', function(msg){
-  console.log(msg);
+sock.on('message', function(a, b){
+  console.log(a, b);
 });
